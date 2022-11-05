251f07343833ebe002f8031edf3336cac940cba2
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2022-11-05 03:58:04 -0300

    [feat]: testing functions

--- querys.sql
+++ querys.sql
@@ -1,22 +1,31 @@
-ALTER TABLE minimarkets_gs;
--- Se crea la nueva columna geo de tipo de dato geography para trabajar distancias
 ALTER TABLE minimarkets_gs ADD COLUMN geo GEOGRAPHY;
--- Se actualiza y se calcula los puntos
+ALTER TABLE minimarkets_gs ADD COLUMN geom GEOMETRY;
 UPDATE minimarkets_gs SET geo = ST_MakePoint(longitud,latitud);
---
-SELECT *
-FROM zonas_censales_gs AS gs;
-SELECT *
-FROM minimarkets_gs AS mini;
---
-SELECT ST_Distance(ST_MakePoint(23.73,37.99),ST_MakePoint(23.73,37.99))
-FROM zonas_censales_gs AS gs, minimarkets_gs AS mini;
---
+UPDATE minimarkets_gs SET geom = ST_SetSRID(ST_MakePoint(longitud,latitud),4326);
+
+ALTER TABLE zonas_censales_gs ADD COLUMN geo GEOGRAPHY;
+ALTER TABLE zonas_censales_gs ADD COLUMN geo2 GEOGRAPHY;
+UPDATE zonas_censales_gs SET geo = ST_Transform(geom,4326)::GEOGRAPHY;
+
+SELECT * FROM minimarkets_gs;
+SELECT * FROM zonas_censales_gs;
+
+SELECT ST_Distance(gs.geo,mini.geo,true) -- Columnas no repetidas
+FROM zonas_censales_gs AS gs, minimarkets_gs AS mini
+LIMIT 1000;
+-- Ver los SRID de las tablas
 SELECT ST_SRID(mini.geo), ST_SRID(gs.geom)
 FROM zonas_censales_gs as gs, minimarkets_gs AS mini;
--- Query 1
-SELECT DISTINCT gs.id, gs.geom -- Columnas no repetidas
-FROM zonas_censales_gs AS gs, minimarkets_gs AS mini
-WHERE NOT(ST_DWithin(ST_Transform(gs.geom,32719),ST_Transform(mini.geo::geometry,32719),500)) -- Transformacion a SRID 32719 y a mas de 500m
-LIMIT 20;
---
+-- Distancia con geography
+SELECT DISTINCT zona_censal.geom
+FROM zonas_censales_gs AS zona_censal, minimarkets_gs AS mini
+WHERE ST_Distance(mini.geo,zona_censal.geo) < 500
+GROUP BY(zona_censal.geom);
+-- Distancia con geometry
+SELECT DISTINCT zona_censal.geom
+FROM zonas_censales_gs AS zona_censal, minimarkets_gs AS mini
+WHERE ST_Distance(
+    ST_Transform(mini.geom::geometry, 32719),
+    zona_censal.geom
+		) > 500
+GROUP BY(zona_censal.geom);
