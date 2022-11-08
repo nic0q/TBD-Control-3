706ed94118265fc511efbf8634d891aa3ab9b44c
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2022-11-08 15:25:29 -0300

    refactor: commentaries and deletion on SELECT sentences

--- querys.sql
+++ querys.sql
@@ -1,17 +1,10 @@
--- Se añade el atributo del punto (geometry)
-ALTER TABLE minimarkets_gs ADD COLUMN geom GEOMETRY;
-UPDATE minimarkets_gs SET geom = ST_SetSRID(ST_MakePoint(longitud,latitud),4326);
-
-SELECT * FROM minimarkets_gs;
-SELECT * FROM zonas_censales_gs;
-
 -- Pregunta 1
-
 -- Se añade el atributo del punto (geometry)
 ALTER TABLE minimarkets_gs ADD COLUMN geom GEOMETRY;
 UPDATE minimarkets_gs SET geom = ST_SetSRID(ST_MakePoint(longitud,latitud),4326);
 
--- Se hace LEFT JOIN con la tabla de zonas censales con un minimarket a menos de 500 metros, obteniendo así la cantidad de minimarkets por zona censal que tienen un minimarket a mas de 500 metros
+-- Se hace LEFT JOIN con las zonas censales que tienen un minimarket en un rango dentro de 500 metros y el total de zonas censales, obteniendo 
+-- las zonas censales que se encuentran a más de 500 metros de algún minimarket
 DROP TABLE IF EXISTS zc_500m;
 CREATE TABLE zc_500m AS(
 	SELECT zona.id, zona.comuna, zona.nom_comuna, zona.geom, zona.cod_distri, zona.cod_zona, zona.geocodigo, zona.shape_leng, zona.shape_area
@@ -20,6 +13,8 @@ CREATE TABLE zc_500m AS(
 	WHERE mini.nombre IS NULL);
 
 -- Pregunta 2
+-- Primero se obtiene una tabla con el total de zonas censales por comuna, luego se hace un RIGTH JOIN con la tabla anterior para obtener el porcentaje 
+-- de zonas censales que se encuentran a más de 500 metros de algún minimarket
 DROP TABLE IF EXISTS comunas_ptje_mini;
 CREATE TABLE comunas_ptje_mini AS(
 	SELECT zonas.nom_comuna, ROUND((COUNT(zc_500m.id) * 100.0) / zonas.total_zonas, 2) AS "porcentaje (%)" , zonas.area_comuna
@@ -30,4 +25,3 @@ CREATE TABLE comunas_ptje_mini AS(
 	ON zonas.comuna = zc_500m.comuna
 	GROUP BY zonas.nom_comuna, zonas.area_comuna, zonas.total_zonas
 	ORDER BY "porcentaje (%)" DESC);
-SELECT * FROM comunas_ptje_mini;
